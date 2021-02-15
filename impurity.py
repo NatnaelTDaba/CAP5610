a883361effce13d477a968f26005b025d76c1de4
NatnaelTDaba <ndaba@alumni.cmu.edu>
2021-02-14 22:39:42 -0500

    Added Gini index and misclassification error

--- impurity.py
+++ impurity.py
@@ -9,14 +9,56 @@ def compute_entropy(class_table):
 
 def compute_children_entropy(class_tables):
 
-    class_tables = np.array(class_tables)
+    class_tables = np.array(class_tables).T
     n = np.sum(class_tables)
-    entropy = sum([(1.0*np.sum(class_tables[:,i])/(n*1.0))*(compute_entropy(class_tables[:,i])) for i in range(len(class_tables))])
+    entropy = sum([(1.0*np.sum(class_tables[:,i])/(n))*(compute_entropy(class_tables[:,i])) for i in range(len(class_tables))])
 
     return entropy
 
-def compute_gain_split(parent_table, children_tables):
+def compute_gain_split_entropy(parent_table, children_tables):
 
     gain = compute_entropy(parent_table) - compute_children_entropy(children_tables)
+
+    return gain
+
+def compute_class_error(class_table):
+
+    pit = 1.0*np.array(class_table)/sum(class_table)
+    class_error = 1 - np.max(pit)
+
+    return class_error
+
+def compute_children_class_error(class_tables):
+
+    class_tables = np.array(class_tables).T
+    n = np.sum(class_tables)
+    class_error = sum([(1.0*np.sum(class_tables[:,i])/n)*(compute_class_error(class_tables[:,i])) for i in range(len(class_tables))])
+
+    return class_error
+
+def compute_gain_split_class_error(parent_table, children_tables):
+
+    gain = compute_class_error(parent_table) - compute_children_class_error(children_tables)
+
+    return gain
+
+def compute_gini(class_table):
+
+    pit = 1.0*np.array(class_table)/sum(class_table)
+    gini_index = 1-np.sum(pit**2)
+
+    return gini_index
+
+def compute_children_gini(class_tables):
+
+    class_tables = np.array(class_tables).T
+    n = np.sum(class_tables)
+    gini_index = sum([(1.0*np.sum(class_tables[:,i])/(n))*(compute_gini(class_tables[:,i])) for i in range(len(class_tables))])
+
+    return gini_index
+
+def compute_gain_split_gini(parent_table, children_tables):
+
+    gain = compute_gini(parent_table) - compute_children_gini(children_tables)
 
     return gain
