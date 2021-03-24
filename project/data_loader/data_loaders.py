2eede0b38ad0446e3e1ce45032c400f39fc16a72
NatnaelTDaba <ndaba@alumni.cmu.edu>
2021-03-24 14:51:12 -0400

    Added comments to utility and loaders

--- project/data_loader/data_loaders.py
+++ project/data_loader/data_loaders.py
@@ -14,22 +14,28 @@ from .datasets import DiabeticRetinopathyDataset
 
 def get_loader(args, test=False):
 
+    # If test loader is desired, create and return DataLoader object for test images.
     if test:
         dataset = DiabeticRetinopathyDataset(config.TEST_IMG_PATH, transforms=config.transforms['test'], test=True)
         test_loader = DataLoader(dataset, batch_size=args.b, shuffle=False)
 
         return test_loader
 
-    
+    # Create training and validation datasets from our custom dataset class DiabeticRetinopathyDataset.
     train_dataset = DiabeticRetinopathyDataset(config.IMG_PATH, config.CSV_PATH, transforms=config.transforms['train'])
     val_dataset = DiabeticRetinopathyDataset(config.IMG_PATH, config.CSV_PATH, transforms=config.transforms['val'])
 
+    # Create random samplers for our training and validation datasets.
     targets = train_dataset.csv['level'].values
+    # Generate a unique set of training and valiadtion indices for sampling. 
+    # Stratified sampling is used so that the validation set has the same class
+    # distribtuion as the training set.
     train_idx, valid_idx= train_test_split(np.arange(len(targets)), test_size=0.2, random_state=42, shuffle=True, stratify=targets)
 
     train_sampler = SubsetRandomSampler(train_idx)
     valid_sampler = SubsetRandomSampler(valid_idx)
 
+    # Create DataLoader object for both sets 
     train_loader = DataLoader(train_dataset, batch_size=args.b, sampler=train_sampler, num_workers=config.NUM_WORKERS)
     val_loader = DataLoader(val_dataset, batch_size=args.b, sampler=valid_sampler, num_workers=config.NUM_WORKERS)
 
--- project/utils.py
+++ project/utils.py
@@ -2,7 +2,12 @@ import pickle
 import config
 
 def save_object(filename, obj):
-    
+
+    """
+        Args:
+            filename (string): Name that the saved file should take
+            obj (object): Object to be saved
+    """
     if filename is None:
         print("Please provide filename.")
     
@@ -11,6 +16,14 @@ def save_object(filename, obj):
     f.close()
 
 def load_object(filename):
+
+     """
+        Args:
+            filename (string): Name file to be loaded
+
+        Returns: loaded object
+
+    """
     
     f = open(config.DATA_DIR+filename, 'rb')
     loaded = pickle.load(f)
